b0d284a5b5437e549ce1584281a57c1131092403
ana-luh <domingos.anaLuh@gmail.com>
2021-06-16 02:47:53 -0300

    enter

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,24 +1,25 @@
+const form = document.querySelector('form');
 const numeroDeSessoes = document.querySelector('#numeroSessoes');
-// console.log(numeroDeSessoes.value)
+const suasSessoes = document.querySelector('#suasSessoes');
+  var objeto =  [{}];
 
+form.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  getData(numeroDeSessoes.value);
+});
 
-
-getArt();
-async function getArt() {
-  let responta = await fetch(`https://api.artic.edu/api/v1/exhibitions?fields=id,title,image_id,alt_image_ids&page=2&limit=5`);
+async function getData(numeroDeSessoes) {
+  let responta = await fetch(`https://api.artic.edu/api/v1/exhibitions?fields=id,title,image_id,alt_image_ids&page=1&limit=${numeroDeSessoes}`);
   let pacote = await responta.json();
   let dados = await pacote.data;
-  for(i=0;i<5;i++){
-    // console.log(dados[i]);
+  // console.log(dados);
+
+  for(i=0;i<numeroDeSessoes;i++){
+    objeto[i] = dados[i]
+    suasSessoes.innerHTML +=  `Título: \t ${dados[i].title} \n ID PARA IMAGEM: \t ${dados[i].image_id} \n\n`;
+    console.log('Este aqui é um novo objeto:\n',dados[i]);
     // console.log(dados[i].id);
     // console.log(dados[i].title);
     // console.log(dados[i].image_id);//Aqui é o código para a imagem
-    // console.log(dados[i].alt_image_ids);
   }
 }
-// getImg();
-// function getImg() {
-// }
-
-
-//https://www.artic.edu/iiif/2/328eb7d6-bddb-d5c1-3fc8-9d8db8aa26fe/full/843,/0/default.jpg
